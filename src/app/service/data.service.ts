2ff5864edc860e57196304bed81548adbba851c4
JobLaptok <grawires@gmail.com>
2022-08-23 19:25:05 +0200

    live data

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -11,8 +11,12 @@ export class DataService {
 
   constructor( private http:HttpClient ) { }
 
+  public liveDataModel: DataModel = {};
   public dataModel: DataModel[] = [];
   public setupModel: SetupModel = {};
+
+  private liveDataModelSubject = new BehaviorSubject<DataModel>(this.liveDataModel);
+  currentLiveDataModel = this.liveDataModelSubject.asObservable();
 
   private dataModelSubject = new BehaviorSubject<DataModel[]>(this.dataModel);
   currentDataModel = this.dataModelSubject.asObservable();
@@ -20,13 +24,27 @@ export class DataService {
   private setupModelSubject = new BehaviorSubject<SetupModel>(this.setupModel);
   currentSetupModel = this.setupModelSubject.asObservable();
 
+  updateLiveDataModel(data: DataModel) {
+    this.liveDataModelSubject.next(data);
+    localStorage.setItem("liveDataModel", JSON.stringify(this.liveDataModel));
+  }
+
   updateDataModel(data: DataModel[]) {
     this.dataModelSubject.next(data);
     localStorage.setItem("dataModel", JSON.stringify(this.dataModel));
   }
+
   updateSetupModel(setup: SetupModel) {
     this.setupModelSubject.next(setup);
     localStorage.setItem("setupModel", JSON.stringify(this.setupModel));
+  }
+
+  public getLiveData(username:String)
+  {
+    return this.http.get<DataModel>( "https://modbus-back.herokuapp.com/live").subscribe( data => {
+        this.updateLiveDataModel(data);
+        this.liveDataModel = data;
+      });
   }
 
   public getData(username:String)
